3614d5d23372f3456eb65cdc77a501f635ae20b6
dayvey <115526062+dayvey@users.noreply.github.com>
2022-11-30 11:57:32 +0200

    Add files via upload

---
+++ 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int sum = 0, prod = 1;
+
+            Console.Write("n=");
+            n=Convert.ToInt32(Console.ReadLine());
+
+            for (int i=1; i<=n; i++)
+            {
+                sum += i;
+                prod *= i;
+            }
+
+            Console.WriteLine("Suma numerelor de la 1 la n este {0} si produsul este {1}", sum, prod);
+        }
+    }
+}
---
+++ 4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, a;
+
+            Console.Write("n=");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("a=");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            int i=0;
+            bool c = true;
+            int j = 0;
+
+            while (n>0)
+            {
+                int x = Convert.ToInt32(Console.ReadLine());
+                if (x == a)
+                {
+                    c = false;
+                    j = i;
+                }
+                else
+                {
+                    i++;
+                }
+                n--;
+            }
+            if (c == true) Console.WriteLine("Numarul a nu era in secventa data (-1)");
+            else Console.WriteLine("Numarul a era pe pozitia {0}", j);
+        }
+    }
+}
