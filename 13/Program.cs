a5088cd48239616f414a5f8bca3a8aa3726ce864
dayvey <115526062+dayvey@users.noreply.github.com>
2022-11-30 12:02:59 +0200

    Add files via upload

---
+++ 13/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.Write("n=");
+            n = int.Parse(Console.ReadLine());
+
+            int x=int.Parse(Console.ReadLine());
+            int y = 0;
+            int c = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                y = int.Parse(Console.ReadLine());
+                if (x > y)
+                    c++;
+                x = y;
+            }
+            if (c <= 1) Console.WriteLine("Secventa data este o secventa crescatoare rotita.");
+            else Console.WriteLine("Secventa data NU este o secventa crescatoare rotita.");
+        }
+    }
+}
---
+++ 14/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            int c = 0;
+
+            if (x > y)
+            {
+                x = y;
+                for (int i = 2; i < n; i++)
+                {
+                    y=Convert.ToInt32(Console.ReadLine());
+                    if (x < y)
+                        c++;
+                    x = y;
+                }
+                if (c <= 1) Console.WriteLine("Secventa data este o secventa monotona rotita.");
+                else Console.WriteLine("Secventa data NU este o secventa monotona rotita.");
+            }
+            else
+            {
+                x = y;
+                for (int i = 2; i < n; i++)
+                {
+                    y = Convert.ToInt32(Console.ReadLine());
+                    if (x > y)
+                        c++;
+                    x = y;
+                }
+                if (c <= 1) Console.WriteLine("Secventa data este o secventa monotona rotita.");
+                else Console.WriteLine("Secventa data NU este o secventa monotona rotita.");
+            }
+        }
+    }
+}
