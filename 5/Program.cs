ca2c959baf7e00e515f7554cb589f3102fe52555
dayvey <115526062+dayvey@users.noreply.github.com>
2022-11-30 11:59:07 +0200

    Add files via upload

---
+++ 5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, nr = 0, x;
+
+            Console.Write("n=");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <n; i++)
+            {
+                x = Convert.ToInt32(Console.ReadLine());
+                if (x == i) nr++;
+            }
+            Console.WriteLine("{0} numere erau egale cu pozitia pe care au aparut", nr);
+        }
+    }
+}
---
+++ 6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.Write("n=");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            bool c = true;
+
+            int y=Convert.ToInt32(Console.ReadLine());
+            n--;
+
+            for (int i=0; i<n; i++)
+            {
+                int x=Convert.ToInt32(Console.ReadLine());
+                if (y > x)
+                    c = false;
+                y = x;
+            }
+            if (c == false)
+                Console.WriteLine("Nu sunt in ordine crescatoare.");
+            else Console.WriteLine("Sunt in ordine crescatoare.");
+        }
+    }
+}
