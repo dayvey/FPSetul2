bb552c5e0f35361491b385e92fa868c0500d34c6
dayvey <115526062+dayvey@users.noreply.github.com>
2022-11-30 11:56:39 +0200

    Add files via upload

---
+++ 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, x;
+            
+            Console.Write("n=");
+            n=int.Parse(Console.ReadLine());
+
+            int num = 0;
+            while (n>0)
+            {
+                x=int.Parse(Console.ReadLine());
+                if (x % 2 == 0)
+                    num++;
+                n -= 1;
+            }
+
+            Console.Write("{0} numere au fost pare.", num);
+        }
+    }
+}
---
+++ 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, x, neg=0, poz=0, zero=0;
+
+            Console.Write("n=");
+            n=Convert.ToInt32(Console.ReadLine());
+
+            while (n > 0)
+            {
+                x=Convert.ToInt32(Console.ReadLine());
+                if (x == 0) zero = zero + 1;
+                else if (x > 0) poz = poz + 1;
+                else neg = neg + 1;
+                n--;
+            }
+            Console.WriteLine("{0} numere au fost negative, {1} numere erau pozitive si {2} era zero", neg, poz, zero);
+        }
+    }
+}
