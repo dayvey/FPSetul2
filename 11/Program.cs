11d0d2a1ee37aff6e49bec62ec9fa3f31cfc7182
dayvey <115526062+dayvey@users.noreply.github.com>
2022-11-30 12:02:06 +0200

    Add files via upload

---
+++ 11/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.Write("n=");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i=1; i<=n; i++)
+            {
+                int x = Convert.ToInt32(Console.ReadLine());
+                int y = 0;
+                while (x > 0)
+                {
+                    y = y * 10 + x % 10;
+                    x /= 10;
+                }
+                sum += y;
+            }
+            Console.WriteLine("Suma inverselor din secventa data de n numere este {0}", sum);
+        }
+    }
+}
---
+++ 12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.Write("n=");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            int num = 0, ant = 0;
+            int x = 0, y = 0;
+
+            for (int i=1; i<=n; i++)
+            {
+                y = x;
+                x = Convert.ToInt32(Console.ReadLine());
+                if (x == 0)
+                    if (ant != 0)
+                        num++;
+                ant = x;
+            }
+            if (x != 0)
+                num++;
+            Console.WriteLine("{0} grupuri de numere consecutive sunt diferite de zero", num);
+        }
+    }
+}
