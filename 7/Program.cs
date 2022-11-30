6fe878a9bda6e5f32b925e0c437b3cf3a9b016cf
dayvey <115526062+dayvey@users.noreply.github.com>
2022-11-30 12:00:15 +0200

    Add files via upload

---
+++ 7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, max, min;
+
+            Console.Write("n=");
+            n = int.Parse(Console.ReadLine());
+
+            int x = int.Parse(Console.ReadLine());
+            max = x; min = x;
+
+            for (int i=1; i<=n-1; i++)
+            {
+                x=int.Parse(Console.ReadLine());
+                if (x > max)
+                    max = x;
+                if (x < min)
+                    min = x;
+            }
+            Console.WriteLine("Cea mai mare valoare este {0} si cea mai mica valoare este {1}", max, min);
+        }
+    }
+}
---
+++ 8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int x, y, z=0;
+
+            Console.Write("n=");
+            n = int.Parse(Console.ReadLine());
+
+            if (n == 0)
+                Console.WriteLine("Al {0}-lea numar din sirul Fibonacci este 0", n);
+            else if (n == 1) Console.WriteLine("Al {0}-lea numar din sirul Fibonacci este 1", n);
+            else
+            {
+                x = 0; y = 1;
+                for (int i=2; i<=n; i++)
+                {
+                    z = x + y;
+                    x = y;
+                    y = z;
+                }
+            }
+            Console.WriteLine("Al {0}-lea numar din sirul Fibonacci este {1}", n, z);
+        }
+    }
+}
