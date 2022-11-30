2b3e756a18fe0f9fd958290b111408a7e7cc1a1a
dayvey <115526062+dayvey@users.noreply.github.com>
2022-11-30 12:01:11 +0200

    Add files via upload

---
+++ 10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.Write("n=");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            int x, y, max, nr;
+            x = Convert.ToInt32(Console.ReadLine());
+            nr = 1;
+            max = 0;
+
+            for (int i=2; i<=n; i++)
+            {
+                y = Convert.ToInt32(Console.ReadLine());
+                if (x != y)
+                {
+                    if (max < nr)
+                        max = nr;
+                    nr = 1;
+                }
+                else nr++;
+                x = y;
+            }
+            if (max < nr)
+                max = nr;
+            Console.WriteLine("Numarul maxim de numere consecutive egale din secventa este {0}", max);
+        }
+    }
+}
---
+++ 9/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, n;
+
+            Console.Write("n=");
+            n = int.Parse(Console.ReadLine());
+
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            bool c = true;
+            if (x > y)
+            {
+                for (int i = 3; i <= n; i++)
+                {
+                    x = int.Parse(Console.ReadLine());
+                    if (y < x)
+                        c = false;
+                    y = x;
+                }
+                if (c == true)
+                    Console.WriteLine("Sirul este monoton descrescatoare.");
+            }
+            else
+            {
+                for (int i = 3; i <= n; i++)
+                {
+                    x = int.Parse(Console.ReadLine());
+                    if (y > x)
+                        c = false;
+                    y = x;
+                }
+                if (c == true)
+                    Console.WriteLine("Sirul este monoton crescatoare.");
+            }
+            if (c == false)
+                Console.WriteLine("Sirul nu este nici monoton crescatoare, nici monoton descrescatoare.");
+        }
+    }
+}
